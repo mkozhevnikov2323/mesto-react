b2f211c5150b69843ec7c938d65763009a3d3dc1
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-05-17 19:18:58 +0400

    dev: add props children to component PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,10 +1,11 @@
 
-function PopupWithForm(props) {
+function PopupWithForm(props, {children}) {
   return (
     <div className={`popup popup_action_${props.name}`}>
       <div className="popup__container">
         <h2 className="popup__title">{`${props.title}`}</h2>
         <form action="#" className="popup__form" name={`${props.name}`} novalidate>
+          {children}
           {/* <input type="text" name="name" className="popup__input popup__input_data_name" value="" required minlength="2" maxlength="40" />
           <span className="popup__error"></span>
           <input type="text" name="about" className="popup__input popup__input_data_job" value="" required minlength="2" maxlength="200" />
