b9b79cc4229727baa3f66870426868b6d15e6ca7
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-05-26 12:55:07 +0400

    dev: add component EditAvatarPopup

---
+++ src/components/EditAvatarPopup.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PopupWithForm from './PopupWithForm';
+
+function EditAvatarPopup({isOpen, onClose, onUpdateAvatar, showLoading}) {
+
+  const editAvatarRef = React.useRef('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onUpdateAvatar({
+      avatar: editAvatarRef.current.value
+    });
+  }
+
+  return (
+    <PopupWithForm
+      name="update-avatar"
+      title="Обновить аватар"
+      submitName={showLoading ? 'Сохранение...' : 'Сохранить'}
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}>
+        <input
+          id="avatar-link"
+          name="avatar"
+          type="url"
+          placeholder="Ссылка на картинку"
+          className="popup__input popup__avatar-link"
+          required
+          ref={editAvatarRef}/>
+        <span
+          className="popup__error popup__error_visible avatar-link-error">
+        </span>
+    </PopupWithForm>
+  );
+}
+
+export default EditAvatarPopup;
