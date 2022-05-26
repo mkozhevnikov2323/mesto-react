bf964e2057eb725f73c4c8ad82017550f1851ed8
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-05-26 12:52:32 +0400

    dev: add component DeletePopup

---
+++ src/components/DeletePopup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PopupWithForm from './PopupWithForm';
+
+function DeletePopup({isOpen, onClose, showLoading, card, onCardDelete}) {
+
+  function handleDeleteClick(e) {
+    e.preventDefault();
+    onCardDelete(card);
+  }
+
+
+  return (
+    <PopupWithForm
+      name="areYouSureToDelete"
+      title="Вы уверены?"
+      submitName={showLoading ? 'Удаление...' : 'Да'}
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleDeleteClick} />
+  );
+}
+
+export default DeletePopup;
