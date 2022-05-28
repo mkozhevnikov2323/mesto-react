49b631bb7f3e4cb2ea19c366bd0bc3064c8c486e
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-05-28 15:09:35 +0400

    hotfix: ref comp ImagePopup without ?:

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ImagePopup({ card, onClose }) {
   return (
-    <div className={ `popup popup_action_show-place ${card.link ? "popup_opened" : ""}` }>
+    <div className={ `popup popup_action_show-place ${card.link && 'popup_opened'}` }>
       <div className="popup__photo-container">
         <img src={ card.link } alt={ card.name } className="popup__photo" />
         <h2 className="popup__photo-title">{ card.name }</h2>
